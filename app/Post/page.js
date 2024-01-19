0e17bd1eba81cd401957734bee63ebf6fc115035
AbsoluteAwesome <152338441+AbsoluteAwesome@users.noreply.github.com>
2024-01-19 20:01:14 +0000

    New Updates

--- app/Post/page.js
+++ app/Post/page.js
@@ -2,12 +2,29 @@
 
 import PostForm from "components/PostForm";
 import { postDoc } from "api/functions/post";
+import { useToast } from "@chakra-ui/react";
 
-export default function page() {
+export default function Page() {
+  const toast = useToast()
   const handleSave = async (data) => {
-    const res = await postDoc(data, "blogs");
-    if (res === true) {
-      alert("Posted");
+    try {
+      const res = await postDoc(data, "blogs");
+      if (res === true) {
+        toast({
+          title: 'Blog Posted.',
+          status: 'success',
+          duration: 9000,
+          isClosable: true,
+        })
+      }
+    } catch (error) {
+      toast({
+        title: 'Something Went Wrong.',
+        description: `Error: ${error}`,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      })
     }
   };
 
